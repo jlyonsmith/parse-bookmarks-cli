2876d6e2c222f50d2de481b88763b44fa67ef205
John Lyon-Smith <john@lyon-smith.org>
2020-08-31 14:19:59 -0700

    Added stream-utils

--- src/ParseBookmarksTool.js
+++ src/ParseBookmarksTool.js
@@ -1,5 +1,10 @@
 import parseArgs from "minimist"
 import { fullVersion } from "./version"
+import fs from "fs"
+import fsPromise from "fs/promises"
+import util from "util"
+import parse from "bookmarks-parser"
+import { stringToStream, pipeToPromise } from "@johnls/stream-utils"
 
 export class ParseBookmarksTool {
   constructor(container) {
@@ -27,7 +32,7 @@ export class ParseBookmarksTool {
 
     if (args.help) {
       this.log.info(`
-Usage: ${this.toolName} [options]
+Usage: ${this.toolName} [options] <bookmarks-file> [<json-file>]
 
 options:
   --help          Shows this help.
@@ -36,7 +41,45 @@ options:
       return 0
     }
 
-    // TODO: Add your code here...
+    if (!args._[0]) {
+      throw new Error("Specify bookmarks file to convert")
+    }
+
+    const bookmarkHtml = await fsPromise.readFile(args._[0], {
+      encoding: "utf-8",
+    })
+    const asyncParseBookmarks = util.promisify(parse)
+    const result = await asyncParseBookmarks(bookmarkHtml)
+    const bookmarks = []
+
+    const processBookmark = (bookmark, path) => {
+      if (bookmark.type === "folder") {
+        bookmark.children.forEach((child) => {
+          processBookmark(child, path + "/" + bookmark.title)
+        })
+      } else {
+        bookmarks.push({
+          title: bookmark.title,
+          url: bookmark.url,
+          path,
+          addedDate: bookmark.add_date,
+          icon: bookmark.icon,
+        })
+      }
+    }
+
+    result.bookmarks.forEach((child) => {
+      processBookmark(child, child.ns_root)
+    })
+
+    this.log.info(`Parsed & flattened ${bookmarks.length} bookmarks`)
+
+    const readable = stringToStream(JSON.stringify(bookmarks))
+    const writeable = args._[1]
+      ? fs.createWriteStream(args._[1])
+      : process.stdout
+
+    await pipeToPromise(readable, writeable)
 
     return 0
   }
